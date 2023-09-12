27a05f5e743fb39ff2d511619f44808ed603c984
maddernd <87599686+maddernd@users.noreply.github.com>
2023-09-12 14:29:40 +1000

    fix: resolved typo in test attempt unit test
    removed typo.
    
    Daniel

--- test/api/numbas/test_attempts_api_test.rb
+++ test/api/numbas/test_attempts_api_test.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
 require 'grape'
 
 RSpec.describe SaveTestAPI, type: :request do
   include TestHelpers::AuthHelper
+  include TestHelpers::TestFileHelper
 
   # Constants
   let(:base_url) { "/savetests" }
